57ac517b85774a0a039d57118c9c52ad6680270b
smartusdc <jakepaul900009@gmail.com>
2025-02-05 08:59:32 +0000

    deploy fix5

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-/** @type {import('next').NextConfig} */
+  /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    webpack: (config) => {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-      return config;
-    },
-  }
+  reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true, // 追加 (1)
+  basePath: '/baseusdcv301', // 追加 (2)
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      dns: false // 追加 (3)
+    };
+    return config;
+  },
+}
